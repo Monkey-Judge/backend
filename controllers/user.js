2b6b7020c16c2aa41aeac4e8512c1f24cfd9e92b
Pkataki <paulogkataki@hotmail.com>
2020-04-01 01:21:23 -0300

    errors fixed, should implement encrytion of the password

--- controllers/user.js
+++ controllers/user.js
@@ -3,25 +3,21 @@
 const userModel = require('../model/user');
 
 
-async function register(req, res, next) {
+function register(req, res, next) {
 	const user = new userModel.User(
 		1, 
 		req.body.login, 
 		req.body.senha, 
 		req.body.email, 
 		0
-	);	
+	);
 
-	try {
-		await userModel.register(user, (err, id) => {
-			if(err)
-				throw new Error(err);
-		});
-		res.status(201).send();
-	} catch(err) {
-		console.log(err);
-		res.status(409).send();
-	}
+	userModel.register(user, (err, id) => {
+		if(!err)
+			res.status(201).send();
+		else
+			res.status(409).send();
+	});
 }
 
 module.exports = {
