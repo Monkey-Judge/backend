daa91eb4068016eb1ffd5d2190add7af6c22d3f6
Rafael Castro <rafaelgyncastro63@gmail.com>
2020-04-27 19:23:39 -0300

    Task model; Updating sql script

--- sql/script.sql
+++ sql/script.sql
@@ -30,8 +30,54 @@ CREATE TABLE `judges` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`),
   UNIQUE KEY `url_UNIQUE` (`url`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `judges`
+--
+
+LOCK TABLES `judges` WRITE;
+/*!40000 ALTER TABLE `judges` DISABLE KEYS */;
+INSERT INTO `judges` VALUES (8,'judge','judge.com.br');
+/*!40000 ALTER TABLE `judges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idjudge` int(11) DEFAULT NULL,
+  `originalId` varchar(45) DEFAULT NULL,
+  `title` varchar(45) NOT NULL,
+  `memoryLimit` int(11) NOT NULL,
+  `timeLimit` int(11) NOT NULL,
+  `statement` varchar(1000) DEFAULT NULL,
+  `input` mediumtext,
+  `output` mediumtext,
+  `notes` mediumtext,
+  `pdfLink` varchar(50) DEFAULT NULL,
+  `sourceLink` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`,`timeLimit`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_tasks_judge_idx` (`idjudge`),
+  CONSTRAINT `fk_tasks_judge` FOREIGN KEY (`idjudge`) REFERENCES `judges` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -46,13 +92,22 @@ CREATE TABLE `users` (
   `password` varchar(45) NOT NULL,
   `salt` varchar(20) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `role` varchar(10) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'user',
   `confirmed` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login_UNIQUE` (`login`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -63,4 +118,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-20 16:28:56
+-- Dump completed on 2020-04-27 19:22:54
