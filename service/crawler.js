530eef472a24da744cc209184ef53571a92d13b5
Rafael Castro <rafaelgyncastro63@gmail.com>
2020-06-01 08:32:47 -0300

    Changing getCodeforcesProblem method to return a Promise

--- service/crawler.js
+++ service/crawler.js
@@ -2,26 +2,30 @@ var cheerio = require('cheerio')
 var request = require('request')
 var task = require('../model/task')
 
-function getCodeforcesProblem (contestId, problem) {
+function getCodeforcesProblem (contestId, problem, callback) {
   const url = `https://codeforces.com/contest/${contestId}/problem/${problem}`
 
-  request({
-    uri: url
-  }, function (error, response, body, callback) {
-    if (error) {
-      return callback(error)
+  return new Promise(
+    (resolve, reject) => {
+      request({
+        uri: url
+      }, function (error, response, body) {
+        if (error) {
+          return reject(error)
+        }
+
+        var $ = cheerio.load(body)
+
+        var title = $('div[class=problem-statement] div[class=title]')
+        var timeLimit = $('div[class=problem-statement] div[class=time-limit]')
+        var memoLimit = $('div[class=problem-statement] div[class=memory-limit]')
+        var statement = $('div[class=problem-statement]')
+
+        return resolve(task.Task(0, 0, contestId + problem, title, memoLimit, timeLimit, statement,
+          '', '', '', '', ''))
+      })
     }
-
-    var $ = cheerio.load(body)
-
-    var title = $('div[class=problem-statement] div[class=title]')
-    var timeLimit = $('div[class=problem-statement] div[class=time-limit]')
-    var memoLimit = $('div[class=problem-statement] div[class=memory-limit]')
-    var statement = $('div[class=problem-statement]')
-
-    return callback(null, task.Task(0, 0, contestId + problem, title, memoLimit, timeLimit, statement,
-      '', '', '', '', ''))
-  })
+  )
 }
 
 module.exports = {
