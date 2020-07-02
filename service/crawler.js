428050b5dc5f34649a4d995ba2470033547674c9
Leandro Vianna <leandro97vianna@gmail.com>
2020-07-02 18:46:20 -0300

    minimal at coder problem fetcher

--- service/crawler.js
+++ service/crawler.js
@@ -2,6 +2,10 @@ var cheerio = require('cheerio')
 var request = require('request')
 var task = require('../model/task')
 
+function filter (text) {
+  return text.replace(/[\n\r]/g, '')
+}
+
 function getCodeforcesProblem (contestId, problem) {
   const url = `https://codeforces.com/contest/${contestId}/problem/${problem}`
 
@@ -24,6 +28,49 @@ function getCodeforcesProblem (contestId, problem) {
   })
 }
 
+function getAtCoderProblem (contestId, problem) {
+  const uri =
+    `https://atcoder.jp/contests/${contestId}/tasks/${problem}`
+
+  return new Promise((resolve, reject) => {
+    request({
+      uri: uri
+    }, function (error, response, body) {
+      if (error != null) {
+        reject(error)
+      }
+
+      const $ = cheerio.load(body)
+
+      // get <title> tag
+      const title = $('title').text().split('-')[1].trim()
+
+      // the limits is on <p>Time Limit: X sec / Memory Limit: X MB</p>
+      const limits = $('p:contains("Time Limit")')
+      const tokens = limits.text().split('/')
+      const timeLimit = Number(tokens[0].trim().split(' ')[2])
+      const memLimit = Number(tokens[1].trim().split(' ')[2]) * 1024 * 1024
+
+      // each part of problem description have the class part
+      const parts = $('.lang-en .part section')
+
+      // remove the h3 tag for each section
+      $('.lang-en .part section h3').remove()
+
+      const statement = filter(parts.eq(0).html() + '<h2>Constraints</h2>' + parts.eq(1).html())
+      const input = filter(parts.eq(2).html())
+      const output = filter(parts.eq(3).html())
+      const sourceLink = uri
+
+      return resolve(
+        new task.Task(0, 0, `${contestId}/${problem}`, title,
+          memLimit, timeLimit, statement, input, output, null, null, sourceLink)
+      )
+    })
+  })
+}
+
 module.exports = {
-  getCodeforcesProblem: getCodeforcesProblem
+  getCodeforcesProblem: getCodeforcesProblem,
+  getAtCoderProblem: getAtCoderProblem
 }
